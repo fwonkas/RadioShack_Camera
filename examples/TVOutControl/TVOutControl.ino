7c64be1157ce07c84992f49e0a557675fd8108e7
Joe Lillibridge <fwonkas@gmail.com>
2018-09-19 17:04:21 -0500

    removed Serial logging from TV out example; oops

--- examples/TVOutControl/TVOutControl.ino
+++ examples/TVOutControl/TVOutControl.ino
@@ -5,12 +5,9 @@ RadioShack_Camera camera;
 void setup() {}
 
 void loop() {
-  Serial.begin(115200);
   delay(3000);
-  Serial.println("tv out off");
   camera.turn_tv_out_off();
 
   delay(3000);
-  Serial.println("tv out on");
   camera.turn_tv_out_on();
 }
